73fd74cc6dae95d02dc0bdfe51c02ea7bebeb10c
Mtsupple <matthewsupple@gmail.com>
2015-05-28 13:03:42 -0400

    Update Swift_Bot.py

--- Swift_Bot.py
+++ Swift_Bot.py
@@ -1,33 +1,49 @@
 import time
 import praw
 import random
+from random import randint
 
-r = praw.Reddit(user_agent = "Test Python Bot /u/Swift_Bot")
+r = praw.Reddit(user_agent = "Test Python Program /u/Swift_Bot")
 r.login('Swift_Bot','1spot1')
 
 print("Logging in...")
 # things to look for in comments
-words_to_match = ['taylor swift','tay tay','t swifty','t-swifty','t swift','t-swifty', 't-swizzle' ]
+
+words_to_match = ["my life sucks", "i hate my life", "i hate everything", "i hate everyone", "fuck everything",
+                  "i hate her", "i hate him","fuck her", "fuck him", "i wish i was dead"]
+
 
 #responses
-Swiftisms = []
+print ("Loading Swiftisms")
+
+#with open("JustSwiftThings.txt", "r+") as swiftisms_file:
+ #   swift_lines = swiftisms_file.readlines()
+  #  swift_list = swift_lines.split('\n')
+
+#swift_quote = swift_list[randint(0, len(swift_list))]
+
+swift_quote = random.choice(open('JustSwiftThings.txt').readlines())
 
 #prevent reposting
 cache = []
 
 def run_bot():
     subreddit = r.get_subreddit("test")
-    comments = subreddit.get_comments(limit=25)
+    comments = subreddit.get_comments(limit=100)
     print('reading comments...')
     for comment in comments:
         comment_text = comment.body.lower()
         isMatch = any(string in comment_text for string in words_to_match)
         if comment.id not in cache and isMatch:
             print('Replying to comment...')
-            comment.reply('test')
+            comment.reply(swift_quote +
+                          "\n \n" +
+                          "\n \n -----------------------------------------------------------------------------------"+
+                          "\n \n \n Now dont you feel better that Taylor Swift cares?")
+
             cache.append(comment.id)
 
 while True:
     run_bot()
-    time.sleep(10)
+    time.sleep(1000)
     print("sleep time")
